428df11d7aca3e7a6fcd19fbde54220c0cc70030
danielas2022 <dzhuxi30@gmail.com>
2024-10-30 22:04:25 -0400

    更新文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://triposrai.com/',  // 替换为您的实际网站 URL
   // 其他配置...
+  integrations: [
+    react({
+      include: ['**/react/*', '**/components/*'],
+      ssr: true
+    }),
+    sitemap()
+  ],
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js']
+    },
+    // 添加 ssr 配置
+    ssr: {
+      noExternal: ['react', 'react-dom']
     }
-  }
+  },
 });
